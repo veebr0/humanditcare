2cf9d0c3850bfbb7c0e15f226de6ee957ee6d5cf
veebr0 <eviny.bracho@gmail.com>
2022-10-06 09:14:13 +0200

    Implemented task_1 this is for zero_special detection

---
+++ task_1/first.py
@@ -0,0 +1,72 @@
+import math
+from random import randint
+
+# Decimal expansion Formula = Sumatory of b*10^k
+# link: https://www.cut-the-knot.org/arithmetic/BaseExpansion.shtml
+# link: https://mathworld.wolfram.com/DecimalExpansion.html
+def get_expansion(num):
+    # split_values represent two lists, means split_values[0] and  split_values[1]
+    # split_values[0] represent the integer part and split_values[1] represent all after .
+    split_values = str(num).split('.')
+    values = list(str(num)) # here convert the number to a list of values
+    exp = len(split_values[0]) -1 # to define the real len just need the the interger part  -1
+    expansion = 0
+    
+    for b in values:
+        if b != '.' :
+            expansion += math.pow(int(b)*10, exp)
+            # print(f'{b}*10^{exp} | resultado:{math.pow(int(b)*10, exp)}')
+            exp -= 1
+    return (expansion) # Return the decimal expansion  acording to the internet documentation
+
+
+# Use zeroes_plus function for test get_expansion() function
+def zeroes_plus(n):
+    real_value = get_expansion(n) # here I get the real value of decimal expansion
+    expansion = list(str(real_value)) #here convert the real value to string and convert it in a list
+    return(expansion.count('0')) # Using .count('0') this will count all elements 0 into the list so zeroes(n) return numbers of 0 into the decimal expansion
+
+
+
+def zeroes(n):
+     expansion = list(str(n)) #here convert the real value to string and convert it in a list
+     return(expansion.count('0')) # Using .count('0') this will count all elements 0 into the list so zeroes(n) return numbers of 0 into the number
+
+
+# Check if number is zero special or not.
+# this funtion return true is zero_special else return false
+def zero_special(n):
+    if zeroes(n) > zeroes(n-1):
+        return True
+    return False
+
+
+
+
+'''
+Nota : test_zero_special is a recursive function just for test some random numbers 
+The following part contain a fuction just to do a test to zero_special
+here just use the magic of recursion, jejejejeje
+'''
+def test_zero_special(n, accumulated_number):
+    # Base case
+    # Return the final state
+    if n >= 300:
+        return accumulated_number
+
+    else:
+        if zero_special(n):
+            print(f'value: {n}, is zero_special...')
+        else:
+            print(f'value: {n}, is not zero_special...')
+        
+        rdn = randint(100, 200)
+       
+        return test_zero_special(n + rdn, accumulated_number + n)
+
+
+
+#Calling the recursive function for a test
+# test_zero_special function need to start with two inital values
+test_zero_special(100, 0)
+
