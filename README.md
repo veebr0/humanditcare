0bf2e47b4b3b96767dfeef1165b131166b162c96
veebr0 <eviny.bracho@gmail.com>
2022-10-07 21:20:35 +0200

    Finished the task 2 and starting with the readme file

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# humanditcare
+# HumandITcare - Technical solutions
+
+## Coding task
+### Define Zeroes, 
+
+
+
+
+## Ship navigation - Part One:
+**Question:** Figure out where the navigation instructions lead. What is the Manhattan distance between that location and the ship's starting position?
+
+**Answer:** Checking with a detail of instructions the navigation computer do not really have a problem, the problem come when the sequence of commands are not correct, check details bellow:
+
+`F10->` Will move the ship 10 units east | resume : boat is at east 10,  north 0. - (good)
+
+`N3->`  Will move the ship 3 units to north - (error)
+
+**Error:** appear the ship is moved 3 units to north , leaving the boat in a position to east 10, north 3, but this is impossible because the boat is still facing to east.
+
+The correct way to turn left the ship is using the command L90 and after F3 so this means that the boat will keep in east 10, north 0.
+
+**Next commands**
+
+`F7->` will move the ship another 7 units east (because the ship is still facing east) to east 17, north 3.
+
+`R90->` will cause the ship to turn right by 90 degrees and face south; it remains at east 17, north 0.
+
+`F11->` will move the ship 11 units south to east 17, south 11.
+
+`Calculation: 17 + 11 = 28`
+
+**Vectors:**
+```python
+a = (10, 0)
+b = (17, 11)
+```
+
+`The correct Manhattan distance: 18`
--- task_2/second.py
+++ task_2/second.py
@@ -1,4 +1,18 @@
+
 import re
+
+
+# some  ways to calculate  Manhattan distances
+
+# Elegand mode jejejejejejejj ,just received two vectors
+def get_manhattan(a, b):
+    return sum(abs(val1-val2) for val1, val2 in zip(a, b))
+
+
+# more complicate
+def get_manhattan2(x1, y1, x2, y2):
+    distance = (x2-x1) + (y2-y1)
+    return distance
 
 
 def get_option(x):
@@ -6,6 +20,9 @@ def get_option(x):
     action = res[0].upper()
     value = res[1]
     return [action, value]
+
+# panel is na fucntion that you can send a value or run empty
+# if you send a value this process the object that need an action
 
 
 def panel(value=""):
@@ -34,70 +51,100 @@ def panel(value=""):
         pass
 
 
-# def get_object_values(obj):
-#     value_x = get_option(obj.x)
-#     value_y = get_option(obj.y)
-#     return [value_x[1], value_y[1]]
-
-
-def getargs(args):
-    for arg in args:
-        print(arg)
-
-
+# Prototype is a class to be able to represent a boat or waypoint
+# this class can contain more method but actually I just create move method
+# this permit move any object however this method need to be improved due to
+# need to cover more option.....
 class Prototype():
     def __init__(self, north, south, east, west, direction,  *args, **kwargs):
         self.north = north
         self.south = south
         self.east = east
         self.west = west
-        self.start_point = 0
-        self.end_point = 0
+        self.start_point = {}
         self.direction = direction.upper()
         self.l = 0
         self.r = 0
 
     def move(self, action, value, *args, **kwargs):
-        if action == 'F' and self.direction == 'E':
+        if action == 'F' and self.direction == waypoint.direction:
             boat.east += value * waypoint.east
             boat.north += value * waypoint.north
-            boat.end_point += boat.east
+
+            if len(self.start_point) == 0:  # check if dict is empty
+                self.start_point = {
+                    'x1':  boat.east,
+                    'y1': boat.north
+                }
+
+        elif action == 'F' and self.direction != waypoint.direction:
+            boat.direction = waypoint.direction
+            boat.east += value * waypoint.east
+            boat.south += (value * waypoint.south) - boat.north
+
+            boat.north = 0
 
         elif action == 'N' and self.direction == 'E':  # actions to move waypoint
             waypoint.north += value
 
         elif action == 'R' and self.direction == 'E':  # actions to move waypoint
-            print('waypoint actual para girar',
-                  f'N{waypoint.north}, S{waypoint.south}, E{waypoint.east}, W{waypoint.west}, direcction={waypoint.direction}, trayectoria={waypoint.end_point}')
 
-        return
+            waypoint.south = waypoint.east
+            waypoint.east = waypoint.north
+            waypoint.direction = 'S'
+            waypoint.north = 0
 
-    def addpos(self, x, y, obj, **kwargs):
-        obj.x = x
-        obj.y = y
         return
 
 
 # waypoint and boat initialization
 # north, south, east, west
-waypoint = Prototype(1, 0, 10, 0, 'E')
-boat = Prototype(0, 0, 0, 0, 'E')
-print('waypoint',
-      f'N{waypoint.north}, S{waypoint.south}, E{waypoint.east}, W{waypoint.west}, direcction={waypoint.direction}, trayectoria={waypoint.end_point}')
-print('boat B',
-      f'N{boat.north}, S{boat.south}, E{boat.east}, W{boat.west}, direcction={boat.direction}, trayectoria={boat.end_point}')
+waypoint = Prototype(1, 0, 10, 0, 'E')  # Initialitation of waypoint
+boat = Prototype(0, 0, 0, 0, 'E')  # Initialitation of boat
 
+
+'''
+The bellow lines just are a representative of the exercice 2
+'''
 panel('F10')
-print('boat A',
-      f'N{boat.north}, S{boat.south}, E{boat.east}, W{boat.west}, direcction={boat.direction}, trayectoria={boat.end_point}')
+print('boat F10',
+      f'N{boat.north}, S{boat.south}, E{boat.east}, W{boat.west}, direcction={boat.direction}')
 
 panel('N3')
-print('waypoint',
-      f'N{waypoint.north}, S{waypoint.south}, E{waypoint.east}, W{waypoint.west}, direcction={waypoint.direction}, trayectoria={waypoint.end_point}')
+print('waypoint N3',
+      f'N{waypoint.north}, S{waypoint.south}, E{waypoint.east}, W{waypoint.west}, direcction={waypoint.direction}')
 
 
 panel('F7')
-print('boat A',
-      f'N{boat.north}, S{boat.south}, E{boat.east}, W{boat.west}, direcction={boat.direction}, trayectoria={boat.end_point}')
+print('boat  F7',
+      f'N{boat.north}, S{boat.south}, E{boat.east}, W{boat.west}, direcction={boat.direction}')
 
 panel('R90')
+print('waypoint R90',
+      f'N{waypoint.north}, S{waypoint.south}, E{waypoint.east}, W{waypoint.west}, direcction={waypoint.direction}')
+
+
+panel('F11')
+print('boat  F11',
+      f'N{boat.north}, S{boat.south}, E{boat.east}, W{boat.west}, direcction={boat.direction}')
+print(
+    f"boat start point - x1: {boat.start_point['x1']} | y1: {boat.start_point['y1']}")
+print(f"boat end   point - x2: {boat.east} | y2: {boat.south}")
+
+
+'''
+To resolved the manhattan distances in task 1
+I just send two vectors. 
+'''
+a = (10, 0)
+# b = (17, 8) wrong calculation
+b = (17, 11)
+print('Manhattan distance for task 1', get_manhattan(a, b))
+
+'''
+To resolved the manhattan distances in task 1
+I just send two vectors. 
+'''
+a = (boat.start_point['x1'], boat.start_point['y1'])
+b = (boat.east, boat.south)
+print('Manhattan distance for task 2', get_manhattan(a, b))
